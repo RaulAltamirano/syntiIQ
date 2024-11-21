ddf2cdc612dcc0a51f3cdc46dc8edfddcc19127e
Pacman <rulomania2011@hotmail.com>
2024-11-20 22:28:19 -0600

    create(tailwind) create new file to tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",        // Si usas Vite o archivos HTML en la raíz
+    "./src/**/*.{vue,js,ts,jsx,tsx}", // Escanea todos los archivos Vue, JS, TS, JSX, y TSX en `src`
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1D4ED8",
+        success: "#4ADE80",
+        warning: "#FBBF24",
+        error: "#F87171",
+      },
+    },
+  },
+  plugins: [],
+
+}
+
