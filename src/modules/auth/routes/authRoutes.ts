b91ad0fc6f4747be9c03caf15011e5c5f2b90c57
Pacman <rulomania2011@hotmail.com>
2024-11-21 22:36:51 -0600

    add(routes) create new routes

--- src/modules/auth/routes/authRoutes.ts
+++ src/modules/auth/routes/authRoutes.ts
@@ -1,8 +1,25 @@
-export default [
-    {
-      path: '/login',
-      name: 'Login',
-      // component: () => import('@/src/module/auth/pages/Login/Login.vue'),
-    },
-  ];
-  
+import { RouteRecordRaw } from 'vue-router';
+import AuthLayout from '../../shared/layouts/AuthLayout/AuthLayout.vue';
+
+
+const authRoutes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    component: AuthLayout,
+    children: [
+      {
+        path: '/login',
+        name: 'Login',
+        component: () => import(/* webpackChunkName: "login-module" */ '../../auth/pages/Login/Login.vue'),
+      },
+      {
+        path: '/register',
+        name: 'Register',
+        component: () => import(/* webpackChunkName: "register-module" */ '../../auth/pages/Register/Register.vue'),
+      },
+    ],
+  },
+];
+
+
+export default authRoutes;
