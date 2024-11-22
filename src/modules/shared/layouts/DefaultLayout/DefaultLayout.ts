208bd5947910d2b326e3e8ba2ec6c5ab15e06fbb
Pacman <rulomania2011@hotmail.com>
2024-11-21 22:37:37 -0600

    create(deault-layout) add new component layout

---
+++ src/modules/shared/layouts/DefaultLayout/DefaultLayout.ts
@@ -0,0 +1,12 @@
+import { defineComponent } from 'vue';
+import Navbar from '../../components/Navbar/Navbar.vue';
+
+export default defineComponent({
+  name: "DefaultLayout",
+  components:{
+    Navbar
+  },
+  setup() {
+
+  },
+});
---
+++ src/modules/shared/layouts/DefaultLayout/DefaultLayout.vue
@@ -0,0 +1,11 @@
+<script lang="ts" src="./DefaultLayout.ts"/>
+
+<template>
+    <div class="flex flex-col min-h-screen">
+      <Navbar />
+      <main class="flex-grow">
+        <RouterView/>
+      </main>
+    </div>
+  </template>
+  
