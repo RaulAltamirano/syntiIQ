5f4bc39730162d7e38636e3f700a962f0051ee73
Pacman <rulomania2011@hotmail.com>
2024-11-22 20:47:34 -0600

    create(authlayout) create layoput

---
+++ src/modules/shared/layouts/AuthLayout/AuthLayout.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="flex flex-col items-center justify-center min-h-screen bg-gray-50">
+      <main class="w-full max-w-md mx-auto">
+        <RouterView/>
+      </main>
+    </div>
+  </template>
+  
