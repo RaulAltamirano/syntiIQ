89fd6e8059b18c0d99a1ec796fa1396343e1c5a6
Pacman <rulomania2011@hotmail.com>
2024-11-22 20:48:43 -0600

    update(routes)add new routes to navegation

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,44 @@
-  import { createRouter, createWebHistory } from 'vue-router';
-import authRoutes from '../modules/auth/routes/authRoutes';
+import { createRouter, createWebHistory } from 'vue-router';
+import { RouteRecordRaw } from 'vue-router';
 import mainRoutes from '../modules/home/router/mainRoutes';
+import authRoutes from '../modules/auth/routes/authRoutes';
+import Login from '../modules/auth/pages/Login/Login.vue';
+
+
+// const errorNotFoundRoute: RouteRecordRaw = {
+//   path: '/:catchAll(.*)*',
+//   component: Home,
+// };
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'Login',
+    component: Login,
+  },
   
-  const routes = [
-    ...authRoutes,
-    ...mainRoutes,
-    { path: '/:catchAll(.*)', redirect: '/login' }, 
-  ];
-  
-  const router = createRouter({
-    history: createWebHistory('example'),
-    routes,
-  });
-  
-  export default router;
-  
+  ...authRoutes,
+  ...mainRoutes,
+
+  {
+    path: '/:catchAll(.*)*',
+    redirect: '/'
+  },
+  // errorNotFoundRoute,
+
+];
+
+// Router instance
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+// Navigation guard (optional but recommended)
+// router.beforeEach((to, from, next) => {
+//   // Add your authentication logic here
+//   console.log(next(), to, from);
+//   next();
+// });
+
+export default router;
