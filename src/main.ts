a40e760986a608e095e4f14e951205b7b049a60d
Pacman <rulomania2011@hotmail.com>
2024-11-24 21:18:21 -0600

    update(main) ad new module

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
 import { createApp } from 'vue'
-import './assets/tailwind.css'; 
-import App from './modules/core/pages/App.vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import './assets/tailwind.css';
 
-createApp(App).mount('#app')
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+
+import App from './modules/core/pages/App.vue'
+import router from './router';
+
+library.add(fas)
+
+createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .use(router)
+    .mount('#app')
