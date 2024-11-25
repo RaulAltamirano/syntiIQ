1c4ecedb3c71911579b2e38308960a7e62856977
Pacman <rulomania2011@hotmail.com>
2024-11-24 21:19:18 -0600

    update(app) add new styles

--- src/modules/core/pages/App.vue
+++ src/modules/core/pages/App.vue
@@ -1,8 +1,14 @@
 <script lang="ts" src="./App.ts"/>
 
 <template>
-  <div id="app">
-    <Navbar />
-    <RouterView />
+  <div id="app" class="min-h-screen">
+    <main>
+      <RouterView />
+    </main>
   </div>
 </template>
+
+
+<!-- <transition name="fade" mode="out-in">
+  <component :is="Component" />
+</transition> -->
