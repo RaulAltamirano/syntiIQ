3809668ae4a11b60272d2e78c67359e6d6fec4b8
Pacman <rulomania2011@hotmail.com>
2024-11-29 09:27:28 -0600

    create(animated-background) crea backgrount to app

---
+++ src/modules/shared/components/Backgound/AnimatedBackground.ts
@@ -0,0 +1,32 @@
+import { onMounted, ref } from 'vue';
+import { useThemeStore } from '../../composables/useTheme';
+import { storeToRefs } from 'pinia';
+
+export default {
+  name: 'AnimatedBackground',
+  components: {
+  },
+  setup() {
+    const themeStore = useThemeStore()
+    const { isDark } = storeToRefs(themeStore)
+    const mousePosition = ref({ x: 0, y: 0 })
+
+    const handleMouseMove = (event: { clientX: any; clientY: any; }) => {
+      mousePosition.value = {
+        x: event.clientX,
+        y: event.clientY
+      }
+    }
+    
+    onMounted(() => {
+      window.addEventListener('mousemove', handleMouseMove)
+    })
+    
+    return {
+        isDark,
+        mousePosition,
+        handleMouseMove,
+
+    }
+  },
+};
