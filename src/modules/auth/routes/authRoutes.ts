00ce434fc5a54cd701a22b8e539663a58725abad
Pacman <rulomania2011@hotmail.com>
2024-12-06 21:49:38 -0600

    update(auth) add new router to login

--- src/modules/auth/routes/authRoutes.ts
+++ src/modules/auth/routes/authRoutes.ts
@@ -1,20 +1,28 @@
 import { RouteRecordRaw } from 'vue-router';
 import AuthLayout from '../../shared/layouts/AuthLayout/AuthLayout.vue';
 
-
 const authRoutes: RouteRecordRaw[] = [
   {
-    path: '/',
+    path: '/auth',
     component: AuthLayout,
+    meta: { requiresAuth: false },
     children: [
       {
-        path: '/login',
+        path: 'login',
         name: 'Login',
+        meta: {
+          requiresAuth: false,
+          title: 'Login'
+        },
         component: () => import(/* webpackChunkName: "login-module" */ '../../auth/pages/Login/Login.vue'),
       },
       {
-        path: '/register',
+        path: 'register',
         name: 'Register',
+        meta: {
+          requiresAuth: false,
+          title: 'Register'
+        },
         component: () => import(/* webpackChunkName: "register-module" */ '../../auth/pages/Register/Register.vue'),
       },
     ],
