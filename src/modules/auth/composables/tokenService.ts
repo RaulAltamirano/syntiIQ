9f93333e114488eade8bbd02a03cf15fedc9b4a1
Pacman <rulomania2011@hotmail.com>
2024-12-07 09:26:41 -0600

    create(token-service) add new tool to token service

--- src/modules/auth/composables/tokenService.ts
+++ src/modules/auth/composables/tokenService.ts
@@ -1,54 +1,38 @@
 import { jwtDecode } from "jwt-decode";
-import { TokenPayload } from "../interfaces/auth.types";
-import { useApi } from "../../services/composables/useApi";
-import { useTokenStorage } from "../../services/composables/useTokenStorage";
+import {  UserSession } from "../interfaces/auth.types";
+import { ref } from "vue";
+import { apiService } from "../../services/api-service";
 
 export const useTokenService = () => {
-  const { getStoredTokens, setTokens } = useTokenStorage();
-  const { api } = useApi<any>();
-  let refreshPromise: Promise<TokenPayload> | null = null;
+  const isLoading = ref(false);
 
   const isTokenExpired = (token: string): boolean => {
     try {
       const decoded = jwtDecode<{ exp: number }>(token);
       const currentTime = Date.now() / 1000;
-      return decoded.exp <= currentTime + 5 * 60; 
+      return decoded.exp <= currentTime + 5 * 60;
     } catch {
       return true;
     }
   };
 
-  const refreshTokens = async (): Promise<TokenPayload> => {
-    console.log('refresh token');
-    if (refreshPromise) return refreshPromise;
 
-    refreshPromise = new Promise(async (resolve, reject) => {
-      try {
-        const currentTokens = getStoredTokens();
-        if (!currentTokens?.refreshToken) {
-          throw new Error('No refresh token available');
-        }
-        const response = await api.post<any>('/auth/refresh-token', {
-          refreshToken: currentTokens.refreshToken
-        })
-
-        if (!response.data) throw new Error('Failed to refresh token');
-
-        const newTokens: TokenPayload = await response.data.tokens
-        setTokens(newTokens);
-        resolve(newTokens);
-      } catch (error) {
-        reject(error);
-      } finally {
-        refreshPromise = null;
-      }
-    });
-
-    return refreshPromise;
+  const verifySession = async () : Promise<UserSession>=> {
+    try {
+      isLoading.value = true;
+      const response = await apiService.get<UserSession>('/auth/me');
+      return response.data;
+    } catch (error) {
+      throw error;
+    } finally {
+      isLoading.value = false;
+    }
   };
+
 
   return {
     isTokenExpired,
-    refreshTokens
+    verifySession,
+    isLoading,
   };
 };
