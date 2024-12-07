b12920ee10f668a27d9a1407882ce6ace689b79c
Pacman <rulomania2011@hotmail.com>
2024-12-07 13:22:22 -0600

    create(auth) ceate new composable

--- src/modules/auth/composables/useAuth.ts
+++ src/modules/auth/composables/useAuth.ts
@@ -1,18 +1,15 @@
 import { computed, ref } from "vue";
 import { useTokenStorage } from "../../services/composables/useTokenStorage";
 import { TokenPayload } from "../interfaces/auth.types";
-import { useApi } from "../../services/composables/useApi";
 import { useNotification } from "../../shared/composables/useNotification";
-import { useTokenService } from "./tokenService";
 import { useRouter } from "vue-router";
 import { useAuthStore } from "../stores/authStore";
+import { apiService } from "../../services/api-service";
 
 export const useAuth = () => {
   const { tokens } = useTokenStorage();
-  const { refreshTokens } = useTokenService();
   const storeAuth = useAuthStore()
   const router = useRouter()
-  const { api } = useApi();
   const notify = useNotification();
   const isAuthenticated = computed(() => !!tokens.value?.accessToken);
   const isLoading = ref(false);
@@ -20,14 +17,13 @@ export const useAuth = () => {
   const login = async (email: string, password: string): Promise<void> => {
     isLoading.value = true;
     try {
-      const response = await api.post<{ tokens: TokenPayload }>('/auth/login', {
+      const response = await apiService.post<{ tokens: TokenPayload }>('/auth/login', {
         email,
         password,
       });
       if (!response.data || !response.data.tokens) {
         throw new Error('Invalid login response');
       }
-      console.log({response});
       storeAuth.setSession(response.data, response.data.tokens);
       router.push({
         name: 'Home'
@@ -41,19 +37,10 @@ export const useAuth = () => {
 
   const logout = (): void => {
     storeAuth.clearSession();
-    notify.info('Logged out');
+    // notify.info('Logged out');
     window.location.href = '/login';
   };
 
-  const handleUnauthorized = async (): Promise<boolean> => {
-    try {
-      await refreshTokens();
-      return true; // Retry successful.
-    } catch {
-      logout(); // Force logout on failure.
-      return false;
-    }
-  };
 
   return {
     isAuthenticated,
@@ -61,6 +48,5 @@ export const useAuth = () => {
     tokens,
     login,
     logout,
-    handleUnauthorized,
   };
 };
