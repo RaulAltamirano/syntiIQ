ec03867607db12d52e75fb59ee5477435fd06b53
Pacman <rulomania2011@hotmail.com>
2024-12-30 10:09:09 -0600

    update(config) -add tsconfig -add tailwind config -add vitestconfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",        // Si usas Vite o archivos HTML en la raíz
-    "./src/**/*.{vue,js,ts,jsx,tsx}", // Escanea todos los archivos Vue, JS, TS, JSX, y TSX en `src`
+    "./index.html",   
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   darkMode: 'class', 
   theme: {
