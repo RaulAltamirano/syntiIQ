2b1ffb65ab17883d870de62d13dfbda1f8dd24c8
Pacman <rulomania2011@hotmail.com>
2025-01-09 22:03:07 -0600

    create(apiConstants) create new file to save constants api

---
+++ src/modules/services/constants/constants-api.ts
@@ -0,0 +1,7 @@
+// constants.ts
+export const API_CONSTANTS = {
+    BASE_URL: 'http://localhost:3000/api',
+    TIMEOUT: 30000,
+    AUTH_HEADER: 'Authorization',
+    BEARER: 'Bearer',
+  } as const;
