a70fe3f3f1820e6225a10f700c85f97e5306e8c8
Pacman <rulomania2011@hotmail.com>
2025-01-25 22:18:46 -0600

    create(DefaultLayout) add new compoennt

--- src/modules/shared/layouts/DefaultLayout/DefaultLayout.ts
+++ src/modules/shared/layouts/DefaultLayout/DefaultLayout.ts
@@ -1,12 +1,20 @@
 import { defineComponent } from 'vue';
 import Navbar from '../../components/Navbar/Navbar.vue';
+import { storeToRefs } from 'pinia';
+import { useThemeStore } from '../../store/ThemeStore';
+import AnimatedBackground from '../../components/Backgound/AnimatedBackground.vue';
 
 export default defineComponent({
   name: "DefaultLayout",
-  components:{
-    Navbar
+  components: {
+    Navbar,
+    AnimatedBackground
   },
   setup() {
-
+    const themeStore = useThemeStore()
+    const { isDark } = storeToRefs(themeStore)
+    return {
+      isDark
+    }
   },
 });
--- src/modules/shared/layouts/DefaultLayout/DefaultLayout.vue
+++ src/modules/shared/layouts/DefaultLayout/DefaultLayout.vue
@@ -1,11 +1,23 @@
 <script lang="ts" src="./DefaultLayout.ts"/>
 
 <template>
-    <div class="flex flex-col min-h-screen">
-      <Navbar />
-      <main class="flex-grow">
-        <RouterView/>
-      </main>
-    </div>
-  </template>
-  
+  <div 
+    class="min-h-screen w-full transition-colors duration-300 overflow-x-hidden"
+    :class="{ 'dark': isDark }"
+  >
+  <AnimatedBackground/>
+
+    <Navbar />
+    <main> 
+      <RouterView v-slot="{ Component }">
+        <Transition
+          name="page"
+          mode="out-in"
+          appear
+        >
+          <component :is="Component" />
+        </Transition>
+      </RouterView>
+    </main>
+  </div>
+</template>
