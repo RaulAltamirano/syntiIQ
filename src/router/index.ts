7df346ae41ad4b34a04820f436e0fa026e3c8d06
Pacman <rulomania2011@hotmail.com>
2025-01-28 22:41:18 -0600

    update(router) add  new routes and remove oldest routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,26 @@
-import { createRouter, createWebHistory, NavigationGuardNext, RouteLocationNormalized } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import { RouteRecordRaw } from 'vue-router';
-import { useAuthStore } from '../modules/auth/stores/authStore';
-import { useTokenService } from '../modules/auth/composables/tokenService';
-import { useTokenStorage } from '../modules/services/composables/useTokenStorage';
 
 import mainRoutes from '../modules/home/router/mainRoutes';
 import authRoutes from '../modules/auth/routes/authRoutes';
 import Login from '../modules/auth/pages/Login/Login.vue';
 import NotFoundRoute from '../modules/shared/components/NotFoundRoute/NotFoundRoute.vue';
 import NotPermissionRoute from '../modules/shared/components/NotPermissionRoute/NotPermissionRoute.vue';
+import LoadingView from '../modules/shared/pages/LoadingView/LoadingView.vue';
+import { setupRouterGuards } from '../modules/auth/guard/setupRouterGuards';
 
 const errorNotFoundRoute: RouteRecordRaw = {
   path: '/:catchAll(.*)*',
-  component: NotFoundRoute ,
+  component: NotFoundRoute,
 };
 const errorNotPermissionRoute: RouteRecordRaw = {
   path: '/403',
-  component: NotPermissionRoute ,
+  component: NotPermissionRoute,
+};
+const LoadingViewRoute: RouteRecordRaw = {
+  path: '/loading',
+  name: 'Loading',
+  component: LoadingView,
 };
 
 const routes: RouteRecordRaw[] = [
@@ -27,6 +31,7 @@ const routes: RouteRecordRaw[] = [
   },
   ...authRoutes,
   ...mainRoutes,
+  LoadingViewRoute,
   errorNotFoundRoute,
   errorNotPermissionRoute,
 
@@ -39,65 +44,6 @@ const router = createRouter({
 });
 
 
-
-// router.beforeEach(async (to: RouteLocationNormalized, from: RouteLocationNormalized, next: NavigationGuardNext) => {
-//   const authStore = useAuthStore();
-//   const { getStoredTokens, clearTokens } = useTokenStorage();
-//   const tokenService = useTokenService();
-  
-//   // Verificar si la ruta requiere autenticación
-//   const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
-//   const requiredRoles = to.meta.roles as string[] | undefined;
-
-//   try {
-//     if (!requiresAuth) {
-//       console.log(to.path);
-//       if (authStore.isAuthenticated && to.path.startsWith('/auth')) {
-//         return next({ name: 'Dashboard' });
-//       }
-//       return next();
-//     }
-
-//     const tokens = getStoredTokens();
-//     if (!tokens) {
-//       console.warn('Tokens no encontrados');
-//       return next({ name: 'Login' }); 
-//     }
-
-//     if (tokenService.isTokenExpired(tokens.accessToken)) {
-//       console.info('Token expirado, intentando refrescar...');
-//       try {
-//         await tokenService.refreshTokens();
-//       } catch (error) {
-//         console.error('Error al renovar el token:', error);
-//         clearTokens();  
-//         return next({ name: 'Login' }); 
-//       }
-//     }
-
-//     // if (requiredRoles?.length) {
-//     //   const hasRequiredRole = requiredRoles.some(role => authStore.hasRole(role));  
-//     //   if (!hasRequiredRole) {
-//     //     console.warn('Acceso denegado: Roles insuficientes');
-//     //     return next({ path: '/403' });  
-//     //   }
-//     // }
-
-//     // Continuar con la navegación si todo es correcto
-//     return next();
-
-//   } catch (error) {
-//     console.error('Error en la protección de ruta:', error);
-//     return next({ name: 'Login' }); 
-//   }
-// });
-
-
-// Navigation guard (optional but recommended)
-// router.beforeEach((to, from, next) => {
-//   // Add your authentication logic here
-//   console.log(next(), to, from);
-//   next();
-// });
+setupRouterGuards(router);
 
 export default router;
