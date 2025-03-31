1ab3243192f6590a64acabc3ac783c299dd8ae20
Pacman <rulomania2011@hotmail.com>
2025-03-31 10:42:30 -0600

    uodate(auth) update types to auth

--- src/modules/auth/interfaces/auth.types.ts
+++ src/modules/auth/interfaces/auth.types.ts
@@ -1,16 +1,19 @@
 export interface UserSession {
+  user: {
     id: string;
     email: string;
     roles: string[];
+    isActive: boolean;
     permissions?: string[];
     tokens: TokenPayload
   }
-  
-  export interface TokenPayload {
-    accessToken: string;
-    refreshToken: string;
-  }
-  export interface AuthError extends Error {
-    code: string;
-    status?: number;
-  }
+}
+
+export interface TokenPayload {
+  accessToken: string;
+  refreshToken: string;
+}
+export interface AuthError extends Error {
+  code: string;
+  status?: number;
+}
